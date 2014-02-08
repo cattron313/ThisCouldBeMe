de7745332c24e38c4b22d2d6c0769218f6597a11
laurenbenichou <laurenbenichou@gmail.com>
2014-02-08 13:44:01 -0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThisCouldBeMe::Application.config.secret_token = 'd80ebd8fb5af56fb2bd4bd3c999aa3452494851856f85f35d32092bc8fd0a89dc0bd47a75274e1925097f9ca0fad0089a9d39fb6fa50da51a348c28de9d74ad3'
